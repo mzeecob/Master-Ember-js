d9b52c00b08cd7d62ed836b4a8bf3f7bbb8e5f6c
CoA Web <devcoaweb@gmail.com>
2019-09-12 18:10:34 +0200

    Merge edit.hbs and new.hbs into form.hbs and use renderTemplate() and setupController()

--- app/routes/libraries/new.js
+++ app/routes/libraries/new.js
@@ -1,10 +1,20 @@
-// app/routes/libraries/new.js
 import Route from '@ember/routing/route';
 
 export default Route.extend({
 
   model() {
     return this.store.createRecord('library');
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('title', 'Create a new library');
+    controller.set('buttonLabel', 'Create');
+  },
+
+  renderTemplate() {
+    this.render('libraries/form');
   },
 
   actions: {
@@ -14,9 +24,11 @@ export default Route.extend({
     },
 
     willTransition() {
-      // rollbackAttributes() removes the record from the store
-      // if the model 'isNew'
-      this.controller.get('model').rollbackAttributes();
+      let model = this.controller.get('model');
+
+      if (model.get('isNew')) {
+        model.destroyRecord();
+      }
     }
   }
 });
