f3530a90afeb83dc52c6579e3fefe350dc675882
CoA Web <devcoaweb@gmail.com>
2019-09-12 18:08:45 +0200

    Cleaning up our templates with components

--- app/models/library.js
+++ app/models/library.js
@@ -1,8 +1,12 @@
 import DS from 'ember-data';
+import { notEmpty } from '@ember/object/computed';
 const { Model, attr } = DS;
 
 export default Model.extend({
   name: attr('string'),
   address: attr('string'),
   phone: attr('string'),
+
+  isValid: notEmpty('name')
+
 });
